d7ae02566ac547c358968ee836fe3f48e6b17b80
Yogesh <coolcoder9334@gmail.com>
2023-11-17 22:47:32 +0530

    Updated the home page and product component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ function Home(){
             setPost(data);
         }catch(error){
             console.log("Error Occoured"); 
-               setPost([]); 
+               setPost([]);  
             } 
         setLoading(false);                  
     } 
@@ -22,14 +22,21 @@ function Home(){
     },[])        
     return(
         <div>  
-            {   
-                loading ? (<Spinner/>):posts.length >0 ? ({
-                    posts.map((post))=>{   
-                        <Product key={post.id} post={post}/>    
-
-                    }   
-                }):();                                  
-            } 
+            {
+        loading ? <Spinner />  :
+        posts.length > 0 ? 
+        (<div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
+        gap-y-8 max-w-6xl p-6 mx-auto my-7 min-h-[80vh]">
+          {
+            posts.map( (post) => (
+            <Product key = {post.id} post={post}/>
+          ) )
+          }
+        </div>) :
+        <div className="w-screen h-screen flex justify-center items-center">
+          <p className="font-bold">No Data Found</p>
+        </div> 
+      }
         </div>
         
     )
